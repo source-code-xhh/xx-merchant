a5b70934cccf52e33ecb906ce66c3213a885a1e1
yumuhunz <1975167000@qq.com>
2023-12-04 16:03:36 +0800

    .

---
+++ assets/merchant-b27dae52.js
@@ -0,0 +1,1 @@
+import{r}from"./el-message-c1f6612d.js";const s=t=>r({url:"/user/user-list",method:"post",data:t}),a=t=>r({url:"/user/add-user",method:"post",data:t}),u=t=>r({url:"/user/edit-user",method:"post",data:t});export{a,u as e,s as m};
